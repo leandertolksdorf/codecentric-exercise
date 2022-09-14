41ae0f5317fa91556fad0c9b2af66938cb475068
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-09-14 11:31:31 +0200

    install and configure jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
