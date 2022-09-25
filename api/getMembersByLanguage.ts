5f13512e128b07c5a95461f054cdae2244fc6983
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-09-25 19:54:43 +0200

    implement getMembersByLanguage

--- api/getMembersByLanguage.ts
+++ api/getMembersByLanguage.ts
@@ -2,6 +2,13 @@ import { fetchMembers } from "./fetchMembers";
 
 export const getMembersByLanguage = async (language?: string) => {
   const members = await fetchMembers();
-  const filteredMembers = members;
+  if (!language) return members;
+
+  const filteredMembers = members.filter(
+    (member) =>
+      member.repositories.findIndex((repository) =>
+        repository.languages.includes(language)
+      ) !== -1
+  );
   return filteredMembers;
 };
